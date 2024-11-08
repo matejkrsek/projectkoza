2bed497e6c9917b150bf15fc667ab0caca97ffef
Matěj Krsek <ikt.krsek@email.cz>
2024-11-08 16:15:20 +0100

    counter

--- src/components/ui/Stepper.jsx
+++ src/components/ui/Stepper.jsx
@@ -1,34 +1,46 @@
 import * as React from 'react';
-
 import { Button } from '@/components/ui';
 import { cn } from '@/lib/utils/cn';
 
 const Stepper = React.forwardRef(
   ({ className, label, onChange, value, ...props }, ref) => {
+    // Funkce pro změnu hodnoty při zadání pouze číselných znaků
+    const handleChange = (e) => {
+      const newValue = e.target.value;
+
+      // Povoluje pouze číslice od 0 do 9
+      if (/^\d*$/.test(newValue)) {
+        onChange(newValue === '' ? '' : Number(newValue));
+      }
+    };
+
     return (
       <div
         className={cn(
-          ' flex h-10 min-w-[150px] items-center justify-between gap-2 rounded-md border border-input bg-background py-2 text-sm',
+          'flex h-10 min-w-[150px] items-center justify-between gap-2 rounded-md border border-input bg-background py-2 text-sm',
           className,
         )}
       >
         <Button
-          disabled={value === 0}
+          disabled={value === 0 || value === ''}
           variant='link'
           onClick={() => onChange(value - 1)}
         >
           -
         </Button>
-        <span
-          className={cn(!value && 'truncate text-muted-foreground')}
+
+        {/* Input pro zadání hodnoty pouze číselných znaků */}
+        <input
+          type='text'
+          value={value === '' ? '' : value} // Zobrazení prázdného pole, pokud je hodnota ''
+          onChange={handleChange}
+          className='h-full w-full border-none bg-transparent text-center outline-none'
           ref={ref}
           {...props}
-        >
-          {value} {label}
-          {value > 1 || value === 0 ? 's' : ''}
-        </span>
+        />
+
         <Button
-          disabled={value === 100}
+          disabled={value === 100 || value === ''}
           variant='link'
           onClick={() => onChange(value + 1)}
         >
@@ -38,6 +50,7 @@ const Stepper = React.forwardRef(
     );
   },
 );
+
 Stepper.displayName = 'Stepper';
 
 export { Stepper };
